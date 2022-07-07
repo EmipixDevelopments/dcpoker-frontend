75acc0ed5ec26d5c59da11932dc2af6eac82ff21
MaksymLytvynenko <fixmonah@gmail.com>
2022-07-07 18:28:06 +0300

    create window MyAccount and MyBonuses

---
+++ Assets/_Script/Panel/Lobby/MyAccount/MyAccountPanelNew.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyAccountPanelNew : MonoBehaviour
+{
+    [SerializeField] private GameObject _panelProfile;
+    [SerializeField] private GameObject _panelMyBonuses;
+
+    // --- test methods ---//
+    public void OpenPanelProfile() 
+    {
+        CloseAll();
+        _panelProfile.SetActive(true);
+    }
+
+    public void OpenPanelMyBonuses() 
+    {
+        CloseAll();
+        _panelMyBonuses.SetActive(true);
+    }
+
+    public void CloseAll() 
+    {
+        _panelProfile.SetActive(false);
+        _panelMyBonuses.SetActive(false);
+    }
+    //----------------------
+}
