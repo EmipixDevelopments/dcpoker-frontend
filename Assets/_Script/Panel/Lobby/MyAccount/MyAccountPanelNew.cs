fa4f6cc00f2fe925d6a8a0ecb9b24c16e89e3301
MaksymLytvynenko <fixmonah@gmail.com>
2022-07-28 21:35:01 +0300

    change logic my profile window

--- Assets/_Script/Panel/Lobby/MyAccount/MyAccountPanelNew.cs
+++ Assets/_Script/Panel/Lobby/MyAccount/MyAccountPanelNew.cs
@@ -4,52 +4,10 @@ using UnityEngine;
 
 public class MyAccountPanelNew : MonoBehaviour
 {
-    [SerializeField] private GameObject _panelProfile;
-    [SerializeField] private GameObject _panelMyBonuses;
-    [SerializeField] private GameObject _panelGameHistory;
-    [SerializeField] private GameObject _panelLeaderBoard;
-    [SerializeField] private GameObject _panelDepositsAndWithdrawals;
-    [SerializeField] private GameObject _panelPurchaseHistory;
+    [SerializeField] private MyAccountMenu _myAccountMenu;
 
-
-    public void OpenPanelProfile() 
+    private void OnEnable()
     {
-        CloseAll();
-        _panelProfile.SetActive(true);
-    }
-    public void OpenPanelMyBonuses() 
-    {
-        CloseAll();
-        _panelMyBonuses.SetActive(true);
-    }
-    public void OpenPanelGameHistory()
-    {
-        CloseAll();
-        _panelGameHistory.SetActive(true);
-    }
-    public void OpenPanelLeaderBoard()
-    {
-        CloseAll();
-        _panelLeaderBoard.SetActive(true);
-    }
-    public void OpenPanelDepositsAndWithdrawals()
-    {
-        CloseAll();
-        _panelDepositsAndWithdrawals.SetActive(true);
-    }
-    public void OpenPanelPurchaseHistory()
-    {
-        CloseAll();
-        _panelPurchaseHistory.SetActive(true);
-    }
-
-    public void CloseAll() 
-    {
-        _panelProfile.SetActive(false);
-        _panelMyBonuses.SetActive(false);
-        _panelGameHistory.SetActive(false);
-        _panelLeaderBoard.SetActive(false);
-        _panelDepositsAndWithdrawals.SetActive(false);
-        _panelPurchaseHistory.SetActive(false);
+        _myAccountMenu.ProfileToggle.isOn = true;
     }
 }
