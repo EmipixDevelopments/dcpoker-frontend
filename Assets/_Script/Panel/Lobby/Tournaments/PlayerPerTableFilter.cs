2974852a1233974f8f7c2aceb5db5eaf0d8d1b49
MaksymLytvynenko <fixmonah@gmail.com>
2022-08-18 11:04:51 +0300

    fixed allToggle in filter players on the table

--- Assets/_Script/Panel/Lobby/Tournaments/PlayerPerTableFilter.cs
+++ Assets/_Script/Panel/Lobby/Tournaments/PlayerPerTableFilter.cs
@@ -1,36 +1,30 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
-using System;
 
 public class PlayerPerTableFilter : MonoBehaviour
 {
     [Header("Lable Panel")]
-    [SerializeField] private GameObject      _lablePanel;
-    [SerializeField] private Button          _labelPanelButton;
+    [SerializeField] private GameObject _lablePanel;
+    [SerializeField] private Button _labelPanelButton;
     [SerializeField] private TextMeshProUGUI _labelText;
     [SerializeField] private TextMeshProUGUI _selectedFilterText;
-    [SerializeField] private GameObject      _arrowImage;
+    [SerializeField] private GameObject _arrowImage;
     [Header("Player Filter Panel")]
     [SerializeField] private GameObject _playerFilterPanel;
-    [SerializeField] private Button     _allButton;
-    [SerializeField] private Toggle     _twoPlayerToggle;
-    [SerializeField] private Toggle     _sixPlayerToggle;
-    [SerializeField] private Toggle     _eightPlayerToggle;
-    [SerializeField] private Toggle     _ninePlayerToggle;
+    [SerializeField] private Toggle _allPlayerToggle;
+    [SerializeField] private Toggle _twoPlayerToggle;
+    [SerializeField] private Toggle _sixPlayerToggle;
+    [SerializeField] private Toggle _eightPlayerToggle;
+    [SerializeField] private Toggle _ninePlayerToggle;
 
     public Action FilterChanged;
 
     private bool _isShowFilter = false;
 
-
-    private string keyTwoPlayerToggle = "TournamentTableSettings.twoPlayerToggle";
-    private string keySixPlayerToggle = "TournamentTableSettings.sixPlayerToggle";
-    private string keyEightPlayerToggle = "TournamentTableSettings.eightPlayerToggle";
-    private string keyNinePlayerToggle = "TournamentTableSettings.ninePlayerToggle";
-
+    private string key = "TournamentTableSettings.PlayerPerTableFilter";
 
     private void OnEnable()
     {
@@ -44,18 +38,19 @@ public class PlayerPerTableFilter : MonoBehaviour
     #region Save/Load
     private void Save()
     {
-        PlayerPrefs.SetInt(keyTwoPlayerToggle, BoolToInt(_twoPlayerToggle.isOn));
-        PlayerPrefs.SetInt(keySixPlayerToggle, BoolToInt(_sixPlayerToggle.isOn));
-        PlayerPrefs.SetInt(keyEightPlayerToggle, BoolToInt(_eightPlayerToggle.isOn));
-        PlayerPrefs.SetInt(keyNinePlayerToggle, BoolToInt(_ninePlayerToggle.isOn));
+        PlayerPrefs.SetInt($"{key}.{_allPlayerToggle.name}", BoolToInt(_allPlayerToggle.isOn));
+        PlayerPrefs.SetInt($"{key}.{_twoPlayerToggle.name}", BoolToInt(_twoPlayerToggle.isOn));
+        PlayerPrefs.SetInt($"{key}.{_sixPlayerToggle.name}", BoolToInt(_sixPlayerToggle.isOn));
+        PlayerPrefs.SetInt($"{key}.{_eightPlayerToggle.name}", BoolToInt(_eightPlayerToggle.isOn));
+        PlayerPrefs.SetInt($"{key}.{_ninePlayerToggle.name}", BoolToInt(_ninePlayerToggle.isOn));
     }
     private void Load()
     {
-        _twoPlayerToggle.isOn = IntToBool(PlayerPrefs.GetInt(keyTwoPlayerToggle, 1));
-        _sixPlayerToggle.isOn = IntToBool(PlayerPrefs.GetInt(keySixPlayerToggle, 1));
-        _eightPlayerToggle.isOn = IntToBool(PlayerPrefs.GetInt(keyEightPlayerToggle, 1));
-        _ninePlayerToggle.isOn = IntToBool(PlayerPrefs.GetInt(keyNinePlayerToggle, 1));
-
+        _allPlayerToggle.isOn = IntToBool(PlayerPrefs.GetInt($"{key}.{_allPlayerToggle.name}", 1));
+        _twoPlayerToggle.isOn = IntToBool(PlayerPrefs.GetInt($"{key}.{_twoPlayerToggle.name}", 1));
+        _sixPlayerToggle.isOn = IntToBool(PlayerPrefs.GetInt($"{key}.{_sixPlayerToggle.name}", 1));
+        _eightPlayerToggle.isOn = IntToBool(PlayerPrefs.GetInt($"{key}.{_eightPlayerToggle.name}", 1));
+        _ninePlayerToggle.isOn = IntToBool(PlayerPrefs.GetInt($"{key}.{_ninePlayerToggle.name}", 1));
     }
     private int BoolToInt(bool value)
     {
@@ -74,38 +69,48 @@ public class PlayerPerTableFilter : MonoBehaviour
         Init();
     }
 
-    public List<int> GetFilterValue() 
+    public List<int> GetFilterValue()
     {
         List<int> result = new List<int>();
-        if (_twoPlayerToggle.isOn) result.Add(2);
-        if (_sixPlayerToggle.isOn) result.Add(6);
-        if (_eightPlayerToggle.isOn) result.Add(8);
-        if (_ninePlayerToggle.isOn) result.Add(9);
+        if (_allPlayerToggle.isOn)
+        {
+            result.Add(2);
+            result.Add(6);
+            result.Add(8);
+            result.Add(9);
+        }
+        else
+        {
+            if (_twoPlayerToggle.isOn) result.Add(2);
+            if (_sixPlayerToggle.isOn) result.Add(6);
+            if (_eightPlayerToggle.isOn) result.Add(8);
+            if (_ninePlayerToggle.isOn) result.Add(9);
+        }
         return result;
     }
 
     private void Init()
     {
         Load();
-        ButtonsAndTogglesRemoveAllListeners();
-        ButtonsAndTogglesAddListeners();
+        RemoveListeners();
+        AddListeners();
         UpdateInfoText();
     }
 
-    private void ButtonsAndTogglesAddListeners()
+    private void AddListeners()
     {
         _labelPanelButton.onClick.AddListener(LabelPanelClick);
-        _allButton.onClick.AddListener(AllFilterClick);
+        _allPlayerToggle.onValueChanged.AddListener(AllFilterClick);
         _twoPlayerToggle.onValueChanged.AddListener(FilterNumberClick);
         _sixPlayerToggle.onValueChanged.AddListener(FilterNumberClick);
         _eightPlayerToggle.onValueChanged.AddListener(FilterNumberClick);
         _ninePlayerToggle.onValueChanged.AddListener(FilterNumberClick);
     }
 
-    private void ButtonsAndTogglesRemoveAllListeners()
+    private void RemoveListeners()
     {
         _labelPanelButton.onClick.RemoveAllListeners();
-        _allButton.onClick.RemoveAllListeners();
+        _allPlayerToggle.onValueChanged.RemoveAllListeners();
         _twoPlayerToggle.onValueChanged.RemoveAllListeners();
         _sixPlayerToggle.onValueChanged.RemoveAllListeners();
         _eightPlayerToggle.onValueChanged.RemoveAllListeners();
@@ -120,18 +125,32 @@ public class PlayerPerTableFilter : MonoBehaviour
         if (_isShowFilter) _playerFilterPanel.SetActive(true);
         else _playerFilterPanel.SetActive(false);
     }
-    private void AllFilterClick()
+    private void AllFilterClick(bool varlue)
     {
-        ButtonsAndTogglesRemoveAllListeners();
+        if (varlue)
+        {
+            RemoveListeners();
 
-        _twoPlayerToggle.isOn = true;
-        _sixPlayerToggle.isOn = true;
-        _eightPlayerToggle.isOn = true;
-        _ninePlayerToggle.isOn = true;
+            _twoPlayerToggle.isOn = false;
+            _sixPlayerToggle.isOn = false;
+            _eightPlayerToggle.isOn = false;
+            _ninePlayerToggle.isOn = false;
 
-        UpdateInfoText();
-        FilterChanged?.Invoke();
-        ButtonsAndTogglesAddListeners();
+            UpdateInfoText();
+            FilterChanged?.Invoke();
+            AddListeners();
+        }
+        else
+        {
+            if (_twoPlayerToggle.isOn == false
+             && _sixPlayerToggle.isOn == false
+             && _eightPlayerToggle.isOn == false
+             && _ninePlayerToggle.isOn == false)
+            {
+                _allPlayerToggle.isOn = true;
+            }
+            return;
+        }
     }
 
     private void FilterNumberClick(bool arg0)
@@ -141,10 +160,18 @@ public class PlayerPerTableFilter : MonoBehaviour
          && _eightPlayerToggle.isOn == false
          && _ninePlayerToggle.isOn == false)
         {
-            AllFilterClick();
+            _allPlayerToggle.isOn = true;
+        }
+        else if (_twoPlayerToggle.isOn == true
+         && _sixPlayerToggle.isOn == true
+         && _eightPlayerToggle.isOn == true
+         && _ninePlayerToggle.isOn == true)
+        {
+            _allPlayerToggle.isOn = true;
         }
         else
         {
+            _allPlayerToggle.isOn = false;
             UpdateInfoText();
             FilterChanged?.Invoke();
         }
@@ -154,17 +181,13 @@ public class PlayerPerTableFilter : MonoBehaviour
     private void UpdateInfoText()
     {
         string selectedFilter = "";
-        if (_twoPlayerToggle.isOn) selectedFilter += " "+2;
-        if (_sixPlayerToggle.isOn) selectedFilter += " " + 6;
-        if (_eightPlayerToggle.isOn) selectedFilter += " " + 8;
-        if (_ninePlayerToggle.isOn) selectedFilter += " " + 9;
-
-        if (_twoPlayerToggle.isOn
-         && _sixPlayerToggle.isOn
-         && _eightPlayerToggle.isOn
-         && _ninePlayerToggle.isOn)
+        if (_allPlayerToggle.isOn) selectedFilter = "ALL";
+        else
         {
-            selectedFilter = "ALL";
+            if (_twoPlayerToggle.isOn) selectedFilter += " " + 2;
+            if (_sixPlayerToggle.isOn) selectedFilter += " " + 6;
+            if (_eightPlayerToggle.isOn) selectedFilter += " " + 8;
+            if (_ninePlayerToggle.isOn) selectedFilter += " " + 9;
         }
         _selectedFilterText.text = selectedFilter;
 
