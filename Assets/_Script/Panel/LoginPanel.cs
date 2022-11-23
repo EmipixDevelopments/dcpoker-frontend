f71c46314903713e1e2777e945d9f52e8aeaf02b
musabhai <menirmalghadiya@gmail.com>
2022-11-23 19:10:04 +0530

    Login Issue (Comment IP Address API call)

--- Assets/_Script/Panel/LoginPanel.cs
+++ Assets/_Script/Panel/LoginPanel.cs
@@ -1,9 +1,11 @@
 ﻿using BestHTTP;
 using System;
 using System.Collections;
+using System.Net;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class LoginPanel : MonoBehaviour
@@ -124,6 +126,20 @@ public class LoginPanel : MonoBehaviour
         OnClickLoginButton(true);
     }
 
+//    public static string GetLocalIPAddress()
+//    {
+//        var host = Dns.GetHostEntry(Dns.GetHostName());
+//        foreach (var ip in host.AddressList)
+//        {
+//            if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+//            {
+//                return ip.ToString();
+//            }
+//        }
+//
+//        throw new Exception("No network adapters with an IPv4 address in the system!");
+//    }
+
     public void OnClickLoginButton(bool forceLoin = false)
     {
         if (CheckDataBeforeLogin())
@@ -138,108 +154,111 @@ public class LoginPanel : MonoBehaviour
                 string password = _password.text;
                 string AuthTokenverify = tokenHack(fullPhoneNumber, password);
                 UIManager.Instance.DisplayLoader("");
-                HTTPRequest httpRequest = new HTTPRequest(new Uri("http://ip-api.com/json"), (request, response) =>
+
+//                HTTPRequest httpRequest = new HTTPRequest(new Uri("http://ip-api.com/json"), (request, response) =>
+//                {
+//                    JSON_Object data = new JSON_Object(response.DataAsText);
+//
+//                    string ipAddress = "NA";
+//                    if (data.has("ip"))
+//                    {
+//                        ipAddress = data.getString("ip");
+//                    }
+//                    else if (data.has("query"))
+//                    {
+//                        ipAddress = data.getString("query");
+//                    }
+//
+//                    Debug.LogError("ipAddress : " + ipAddress);
+
+                // login and password not used
+                UIManager.Instance.SocketGameManager.Login(fullPhoneNumber, password, AuthTokenverify, "", forceLoin, (socket, packet, args) =>
                 {
-                    JSON_Object data = new JSON_Object(response.DataAsText);
+                    Debug.Log("login = " + packet.ToString());
+                    UIManager.Instance.HideLoader();
+                    JSONArray arr = new JSONArray(packet.ToString());
+                    string Source;
+                    Source = arr.getString(arr.length() - 1);
+                    var resp = Source;
 
-                    string ipAddress = "NA";
-                    if (data.has("ip"))
+                    PokerEventResponse<PlayerLoginResponse> loginResponse = JsonUtility.FromJson<PokerEventResponse<PlayerLoginResponse>>(resp);
+                    if (loginResponse.status.Equals(Constants.PokerAPI.KeyStatusSuccess))
                     {
-                        ipAddress = data.getString("ip");
-                    }
-                    else if (data.has("query"))
-                    {
-                        ipAddress = data.getString("query");
-                    }
+                        UIManager.Instance.webglToken = loginResponse.result.deviceId;
+                        UIManager.Instance.IsMultipleTableAllowed = loginResponse.result.isMultipleTableAllowed;
+                        UIManager.Instance.isChipsTransferAllowed = loginResponse.result.isChipsTransferAllowed;
+                        UIManager.Instance.assetOfGame.SavedLoginData.chips = loginResponse.result.chips;
+                        UIManager.Instance.assetOfGame.SavedLoginData.PlayerId = loginResponse.result.id;
+                        UIManager.Instance.assetOfGame.SavedLoginData.PlayerId = loginResponse.result.playerId;
+                        UIManager.Instance.assetOfGame.SavedLoginData.SelectedAvatar = loginResponse.result.profilePic;
+                        UIManager.Instance.assetOfGame.SavedLoginData.isCash = loginResponse.result.isCash;
+                        UIManager.Instance.assetOfGame.SavedLoginData.isSuperPlayer = loginResponse.result.isSuperPlayer;
+                        UIManager.Instance.MySuperPlayer = loginResponse.result.isSuperPlayer;
 
-                    // login and password not used
-                    UIManager.Instance.SocketGameManager.Login(fullPhoneNumber, password, AuthTokenverify, ipAddress, forceLoin, (socket, packet, args) =>
-                    {
-                        Debug.Log("login = " + packet.ToString());
-                        UIManager.Instance.HideLoader();
-                        JSONArray arr = new JSONArray(packet.ToString());
-                        string Source;
-                        Source = arr.getString(arr.length() - 1);
-                        var resp = Source;
+                        UIManager.Instance.assetOfGame.SavedLoginData.isInAppPurchaseAllowed = loginResponse.result.isInAppPurchaseAllowed;
+                        UIManager.Instance.ProfilePic = UIManager.Instance.assetOfGame.SavedLoginData.SelectedAvatar;
+                        UIManager.Instance.assetOfGame.SavedLoginData.Username = loginResponse.result.username; //LoginUsername.text;
 
-                        PokerEventResponse<PlayerLoginResponse> loginResponse = JsonUtility.FromJson<PokerEventResponse<PlayerLoginResponse>>(resp);
-                        if (loginResponse.status.Equals(Constants.PokerAPI.KeyStatusSuccess))
+                        UIManager.Instance.assetOfGame.SavedLoginData.password = _password.text;
+                        UIManager.Instance.assetOfGame.SavedLoginData.phoneNumber = _phoneNumber.text;
+                        UIManager.Instance.assetOfGame.SavedLoginData.phoneCode = _phoneCodeDropdown.options[_phoneCodeDropdown.value].text;
+
+                        UIManager.Instance.assetOfGame.SavedLoginData.userUuid = loginResponse.result.userUuid;
+                        UIManager.Instance.assetOfGame.SavedLoginData.accountNumber = loginResponse.result.accountNumber;
+                        UIManager.Instance.assetOfGame.SavedLoginData.mobile = loginResponse.result.mobile;
+                        if (_rememberMeToggle.isOn)
                         {
-                            UIManager.Instance.webglToken = loginResponse.result.deviceId;
-                            UIManager.Instance.IsMultipleTableAllowed = loginResponse.result.isMultipleTableAllowed;
-                            UIManager.Instance.isChipsTransferAllowed = loginResponse.result.isChipsTransferAllowed;
-                            UIManager.Instance.assetOfGame.SavedLoginData.chips = loginResponse.result.chips;
-                            UIManager.Instance.assetOfGame.SavedLoginData.PlayerId = loginResponse.result.id;
-                            UIManager.Instance.assetOfGame.SavedLoginData.PlayerId = loginResponse.result.playerId;
-                            UIManager.Instance.assetOfGame.SavedLoginData.SelectedAvatar = loginResponse.result.profilePic;
-                            UIManager.Instance.assetOfGame.SavedLoginData.isCash = loginResponse.result.isCash;
-                            UIManager.Instance.assetOfGame.SavedLoginData.isSuperPlayer = loginResponse.result.isSuperPlayer;
-                            UIManager.Instance.MySuperPlayer = loginResponse.result.isSuperPlayer;
+                            UIManager.Instance.SetPlayerLoginType(1);
+                        }
 
-                            UIManager.Instance.assetOfGame.SavedLoginData.isInAppPurchaseAllowed = loginResponse.result.isInAppPurchaseAllowed;
-                            UIManager.Instance.ProfilePic = UIManager.Instance.assetOfGame.SavedLoginData.SelectedAvatar;
-                            UIManager.Instance.assetOfGame.SavedLoginData.Username = loginResponse.result.username; //LoginUsername.text;
+                        UIManager.Instance.assetOfGame.SavedLoginData.isRememberMe = _rememberMeToggle.isOn;
+                        SaveLoad.SaveGame();
+                        //------------------//
 
-                            UIManager.Instance.assetOfGame.SavedLoginData.password = _password.text;
-                            UIManager.Instance.assetOfGame.SavedLoginData.phoneNumber = _phoneNumber.text;
-                            UIManager.Instance.assetOfGame.SavedLoginData.phoneCode = _phoneCodeDropdown.options[_phoneCodeDropdown.value].text;
-
-                            UIManager.Instance.assetOfGame.SavedLoginData.userUuid = loginResponse.result.userUuid;
-                            UIManager.Instance.assetOfGame.SavedLoginData.accountNumber = loginResponse.result.accountNumber;
-                            UIManager.Instance.assetOfGame.SavedLoginData.mobile = loginResponse.result.mobile;
-                            if (_rememberMeToggle.isOn)
-                            {
-                                UIManager.Instance.SetPlayerLoginType(1);
-                            }
-
-                            UIManager.Instance.assetOfGame.SavedLoginData.isRememberMe = _rememberMeToggle.isOn;
-                            SaveLoad.SaveGame();
-                            //------------------//
-
-                            //						UIManager.Instance.assetOfGame.SavedLoginData.isCash= false;
-                            if (UIManager.Instance.assetOfGame.SavedLoginData.isCash)
-                            {
-                                UIManager.Instance.currencyType = UIManager.CurrencyType.cash;
-                            }
-                            else
-                            {
-                                UIManager.Instance.currencyType = UIManager.CurrencyType.coin;
-                            }
-
-                            UIManager.Instance.SetCurrencyImages();
-                            UIManager.Instance.ipLocationService.SendIPAddress("login");
-
-                            UIManager.Instance.LobbyPanelNew.Open(); // LobbyScreeen not used more
-                            UIManager.Instance.MainHomeScreen.Close();
+                        //						UIManager.Instance.assetOfGame.SavedLoginData.isCash= false;
+                        if (UIManager.Instance.assetOfGame.SavedLoginData.isCash)
+                        {
+                            UIManager.Instance.currencyType = UIManager.CurrencyType.cash;
                         }
                         else
                         {
-                            if (loginResponse.message == "updateApp")
-                            {
-                                UIManager.Instance.DisplayConfirmationPanel("Please update the game By click on Update", "Update", "Exit Game",
-                                    () => { Application.OpenURL(loginResponse.result.storeUrl); },
-                                    () => { Application.Quit(); });
-                            }
-                            else if (loginResponse.message == "alreadyLogin")
-                            {
-                                UIManager.Instance.DisplayConfirmationPanel("Same player is already logged in from another device, are you sure you want to login?", "Login", "Cancel",
-                                    () =>
-                                    {
-                                        UIManager.Instance.messagePanel.Close();
-                                        OnClickLoginButton(true);
-                                    }, () => { UIManager.Instance.messagePanel.Close(); });
-                            }
-                            else if (loginResponse.status == "forceLogout")
-                            {
-                            }
-                            else
-                            {
-                                UIManager.Instance.DisplayMessagePanel(loginResponse.message, null);
-                            }
+                            UIManager.Instance.currencyType = UIManager.CurrencyType.coin;
                         }
-                    });
+
+                        UIManager.Instance.SetCurrencyImages();
+                        UIManager.Instance.ipLocationService.SendIPAddress("login");
+
+                        UIManager.Instance.LobbyPanelNew.Open(); // LobbyScreeen not used more
+                        UIManager.Instance.MainHomeScreen.Close();
+                    }
+                    else
+                    {
+                        if (loginResponse.message == "updateApp")
+                        {
+                            UIManager.Instance.DisplayConfirmationPanel("Please update the game By click on Update", "Update", "Exit Game",
+                                () => { Application.OpenURL(loginResponse.result.storeUrl); },
+                                () => { Application.Quit(); });
+                        }
+                        else if (loginResponse.message == "alreadyLogin")
+                        {
+                            UIManager.Instance.DisplayConfirmationPanel("Same player is already logged in from another device, are you sure you want to login?", "Login", "Cancel",
+                                () =>
+                                {
+                                    UIManager.Instance.messagePanel.Close();
+                                    OnClickLoginButton(true);
+                                }, () => { UIManager.Instance.messagePanel.Close(); });
+                        }
+                        else if (loginResponse.status == "forceLogout")
+                        {
+                        }
+                        else
+                        {
+                            UIManager.Instance.DisplayMessagePanel(loginResponse.message, null);
+                        }
+                    }
                 });
-                httpRequest.Send();
+//                });
+//                httpRequest.Send();
             }
         }
     }
